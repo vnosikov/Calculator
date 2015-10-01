acfdc3de5ea34ddbceb03c7f3d4b25e161e7330f
vnosikov <vasily-nosikov@yandex.ru>
2015-10-01 14:57:54 +0300

    Calculator's buttons work now

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
 	$('.equals').click(execute);
+	$('.calc input[type="button"]:not(.equals):not(.backspace)').click(input);
+	$('.backspace').click(backspace);
 });
+
+function input(){
+	var statementField = document.getElementById('statement');
+	statementField.value += this.value;
+}
+
+function backspace(){
+	var statementField = document.getElementById('statement');
+	statementField.value = statementField.value.slice(0,-1);
+}
 
 function execute(){
 	var statementField = document.getElementById('statement');
