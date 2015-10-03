f1e31dd42503de6fac2dc6428e6deb1aaeaf61bf
vnosikov <vasily-nosikov@yandex.ru>
2015-10-03 21:53:08 +0300

    disabled buttons visual embellishment; enter press interception

--- app.js
+++ app.js
@@ -2,21 +2,28 @@ $(document).ready(function(){
 	$('.equals').click(execute);
 	$('.calc input[type="button"]:not(.equals):not(.backspace)').click(input);
 	$('.backspace').click(backspace);
+	
+	$('input').on('keypress', function(e){
+		if(e.keyCode == 13){
+			e.preventDefault();
+			execute();
+		}
+	});
 });
 
 function input(){
-	var statementField = document.getElementById('statement');
-	statementField.value += this.value;
+	var $statement = $('#statement')[0];
+	$statement.value += this.value;
 }
 
 function backspace(){
-	var statementField = document.getElementById('statement');
-	statementField.value = statementField.value.slice(0,-1);
+	var $statement = $('#statement')[0];
+	$statement.value = statementField.value.slice(0,-1);
 }
 
 function execute(){
-	var statementField = document.getElementById('statement');
-	var s = statementField.value;
+	var $statement = $('#statement')[0];
+	var s = $statement.value;
 	var array = parseStatement(s);
 	statementField.value = reduceArray(array);
 }
